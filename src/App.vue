49b0a4fd0b62ae5201880ca5c2ede7158d980b8c
Shugen <shugen002@users.noreply.github.com>
2022-04-03 13:47:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
