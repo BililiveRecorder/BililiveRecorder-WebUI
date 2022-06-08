aeed62d2f490efcf55051341441f274fd2f92a71
genteure <genteure@gmail.com>
2022-06-08 22:31:26 +0800

    feat: remove footer

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,19 @@
     <global-provider>
       <n-layout :style="{ height: vh100 + 'px' }">
         <top-bar />
-        <n-layout position="absolute" style="top: 64px; bottom: 64px" has-sider>
+        <n-layout position="absolute" style="top:64px" has-sider>
           <side-menu></side-menu>
           <n-layout content-style="padding: 24px;height: 100%;" :native-scrollbar="false">
             <router-view></router-view>
           </n-layout>
         </n-layout>
-        <n-layout-footer position="absolute"
-          style="height: 64px;display: flex; justify-content: flex-start; align-items: center" bordered>Copyright (C)
-          2022
-          Shugen002. GPL v3.0</n-layout-footer>
       </n-layout>
     </global-provider>
   </n-message-provider>
 </template>
 
 <script setup lang="ts">
-import { NLayout, NLayoutFooter, NMessageProvider } from 'naive-ui';
+import { NLayout, NMessageProvider } from 'naive-ui';
 import { onMounted, onUnmounted, ref } from 'vue';
 import GlobalProvider from './components/GlobalProvider.vue';
 import SideMenu from './components/SideMenu.vue';
