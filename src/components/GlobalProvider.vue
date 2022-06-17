703cc8395b58d59b64519242375ba086806f1603
Shugen <shugen002@users.noreply.github.com>
2022-06-18 01:06:14 +0800

    feat: locale zhcn

--- src/components/GlobalProvider.vue
+++ src/components/GlobalProvider.vue
@@ -1,12 +1,13 @@
 <template>
-  <n-config-provider :theme="darkTheme">
+  <n-config-provider :theme="darkTheme" :locale="zhCN" :date-locale="dateZhCN">
     <n-loading-bar-provider>
       <n-dialog-provider>
-          <slot></slot>
+        <slot></slot>
       </n-dialog-provider>
     </n-loading-bar-provider>
   </n-config-provider>
 </template>
 <script setup lang="ts">
 import { NConfigProvider, NLoadingBarProvider, NDialogProvider, darkTheme } from 'naive-ui';
+import { zhCN, dateZhCN } from 'naive-ui';
 </script>
