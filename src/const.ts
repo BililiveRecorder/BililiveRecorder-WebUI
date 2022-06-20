37a6da7dce29ac5e2905847c6f85ea51434df1a3
Shugen <shugen002@users.noreply.github.com>
2022-06-21 01:23:51 +0800

    fix: DEV should be always false

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
 export const VERSION = __VERSION__ || 'dev';
 export const BASE_URL = __BASE_URL__ || '/';
 export const EMBEDDED_BUILD = __EMBEDDED_BUILD__ || false;
-export const DEV = __DEV__ || true;
+export const DEV = __DEV__ || false;
