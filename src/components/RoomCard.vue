6047812722de78f160a9703d233f61d37ebe5f80
Shugen <shugen002@users.noreply.github.com>
2022-06-28 13:11:06 +0800

    feat: show all stat data

--- src/components/RoomCard.vue
+++ src/components/RoomCard.vue
@@ -50,26 +50,16 @@
     </div>
     <div class="record-status">
       <div class="recording" v-if="props.room.recording">
-        <n-icon size="20" :component="RecordingOutline" color="#a00" />录制中
+        <n-icon size="20" :component="RecordingOutline" color="#a00" />
+        录制中({{ msToHuman(props.room.recordingStats.sessionMaxTimestamp) }})
       </div>
       <div class="recording" v-show="!props.room.recording && props.room.autoRecord">
         <n-icon size="20" :component="Analytics" :color="themeVars.warningColor" />监控中
       </div>
-      <n-popover class="network" v-if="props.room.recording" :delay="500" :duration="500" :show="isPopoverShow"
-        @update:show="handlePopoverVisibleChange">
-        <template #trigger>
-          <n-gradient-text :type="statColor(props.room.recordingStats.durationRatio)">
-            {{ props.room.ioStats.networkMbps.toFixed(2) }} Mbps
-          </n-gradient-text>
-        </template>
-        <div class="stat">
-          <p>下载速度：{{ stat.networkMbps.toFixed(2) }} Mbps</p>
-          <p>录制速度比例：{{ (stat.durationRatio * 100).toFixed(2) }} %</p>
-          <p>文件大小：{{ byteToHuman(stat.currentFileSize) }}</p>
-          <p>会话时长：{{ msToHuman(stat.sessionDuration) }}</p>
-          <p>已录制时长：{{ msToHuman(stat.sessionMaxTimestamp) }}</p>
-        </div>
-      </n-popover>
+      <n-gradient-text v-if="props.room.recording" :type="statColor(props.room.recordingStats.durationRatio)"
+        @click="emit('show-stats')">
+        {{ props.room.ioStats.networkMbps.toFixed(2) }} Mbps
+      </n-gradient-text>
     </div>
     <n-modal :title="'房间设置 ' + props.room.roomId" v-model:show="showSettingDialog" preset="card"
       :style="{ maxWidth: '800px', maxHeight: '95vh' }">
@@ -132,10 +122,10 @@
   </n-card>
 </template>
 <script setup lang="ts">
-import { Component, h, onMounted, onUnmounted, PropType, ref, watch } from 'vue';
+import { Component, h, onMounted, onUnmounted, PropType, ref } from 'vue';
 import {
   DropdownOption, useLoadingBar, useMessage, NCollapseTransition, NCard, NH3, NIcon,
-  NSpace, NTooltip, NDropdown, NButton, NModal, NSkeleton, NPopover, useThemeVars,
+  NSpace, NTooltip, NDropdown, NButton, NModal, NSkeleton, useThemeVars,
   NGradientText,
 } from 'naive-ui';
 import {
@@ -143,7 +133,7 @@ import {
   PlayCircle, StopCircle, Refresh, Open, Settings, Trash,
 } from '@vicons/ionicons5';
 import { Recorder, RoomDto, Optional } from '../utils/api';
-import { byteToHuman, msToHuman } from '../utils/unitConvert';
+import { msToHuman } from '../utils/unitConvert';
 import OptionalInput from '../components/OptionalInput.vue';
 import { recorderController } from '../utils/RecorderController';
 
@@ -319,7 +309,7 @@ const actions: DropdownOption[] = [
 ];
 
 const emit = defineEmits(['start-record', 'stop-record', 'refresh-room-info', 'start-auto-record',
-  'stop-auto-record', 'delete', 'self-update']);
+  'stop-auto-record', 'delete', 'self-update', 'show-stats']);
 const showSettingDialog = ref(false);
 const loading = ref(true);
 
@@ -473,84 +463,6 @@ function statColor(ratio: number) {
   return 'error';
 }
 
-const stat = ref({
-  networkMbps: 0,
-  durationRatio: 0,
-  currentFileSize: 0,
-  sessionDuration: 0,
-  sessionMaxTimestamp: 0,
-});
-
-// these two will be update by outside, so we need to watch them
-watch(props.room.ioStats, (newVal) => {
-  stat.value.networkMbps = newVal.networkMbps;
-});
-watch(props.room.recordingStats, (newVal) => {
-  stat.value.durationRatio = newVal.durationRatio;
-});
-
-
-interface QueryResult {
-  r: {
-    i: {
-      n: number;
-    },
-    r: {
-      r: number;
-      s: number;
-      d: number;
-      t: number;
-    }
-  }
-}
-
-function pullStat() {
-  if (recorderController.recorder == null) {
-    return;
-  }
-  recorderController.recorder.graphql<QueryResult>('q',
-    'query q($o:ID){r:room(objectId:$o){i:ioStats{n:networkMbps}r:recordingStats{r:durationRatio s:currentFileSize d:sessionDuration t:sessionMaxTimestamp}}}',
-    { o: props.room.objectId })
-    .then((data) => {
-      stat.value.networkMbps = data.r.i.n;
-      stat.value.durationRatio = data.r.r.r;
-      stat.value.currentFileSize = data.r.r.s;
-      stat.value.sessionDuration = data.r.r.d;
-      stat.value.sessionMaxTimestamp = data.r.r.t;
-    })
-    .catch((error) => {
-      // TODO: 错误处理
-      console.error(error);
-    });
-}
-
-const isPopoverShow = ref(false);
-let pullStatInterval: number | null = null;
-function handlePopoverVisibleChange(visible: boolean) {
-  isPopoverShow.value = visible;
-  if (visible) {
-    pullStat();
-    if (pullStatInterval) {
-      clearInterval(pullStatInterval);
-    }
-    pullStatInterval = setInterval(() => {
-      if (document.hidden) {
-        return;
-      }
-      pullStat();
-    }, 1000);
-  } else {
-    if (pullStatInterval) {
-      clearInterval(pullStatInterval);
-    }
-    pullStatInterval = null;
-  }
-}
-onUnmounted(() => {
-  if (pullStatInterval) {
-    clearInterval(pullStatInterval);
-  }
-});
 </script>
 
 <style scoped lang="scss">
@@ -573,9 +485,17 @@ onUnmounted(() => {
   margin-bottom: 24px;
 }
 
-.stat {
-  >p {
-    margin: 0;
-  }
+.n-gradient-text {
+  font-weight: bold;
+  // no, it works, but bad cpu usage
+  // background-size: 200% auto;
+  // background-image: linear-gradient(252deg, var(--n-color-start) 0%, var(--n-color-end) 50%, var(--n-color-start) 100%);
+  // animation: 1s roll linear infinite forwards;
 }
+
+// @keyframes roll {
+//   to {
+//     background-position: 200% center;
+//   }
+// }
 </style>
--- src/utils/unitConvert.ts
+++ src/utils/unitConvert.ts
@@ -9,15 +9,22 @@ export function byteToHuman(bytes: number): string {
   return `${(bytes / Math.pow(1024, unitIndex)).toFixed(2)} ${units[unitIndex]}`;
 }
 
-export function msToHuman(ms:number): string {
-  const time=[0, 0, 0, 0, 0];
+export function msToHuman(ms: number, includeMs: boolean = false): string {
+  const time = [0, 0, 0, 0, 0];
   time[0] = Math.floor(ms / 1000 / 60 / 60 / 24);
-  time[1] = Math.floor(ms / 1000 / 60 / 60 % 24);
-  time[2] = Math.floor(ms / 1000 / 60 % 60);
-  time[3] = Math.floor(ms / 1000 % 60);
+  time[1] = Math.floor((ms / 1000 / 60 / 60) % 24);
+  time[2] = Math.floor((ms / 1000 / 60) % 60);
+  time[3] = Math.floor((ms / 1000) % 60);
   time[4] = Math.floor(ms % 1000);
-  return (time[0]>0?(time[0]+':'):'') +
-    (time[1]>9?time[1]:('0'+time[1])) + ':' +
-    (time[2]>9?time[2]:('0'+time[2])) + ':' +
-    (time[3]>9?time[3]:('0'+time[3])) + '.' + time[4];
+  return (
+    (time[0] > 0 ? time[0] + ':' : '') +
+    (time[1] > 9 ? time[1] : '0' + time[1]) + ':' +
+    (time[2] > 9 ? time[2] : '0' + time[2]) + ':' +
+    (time[3] > 9 ? time[3] : '0' + time[3]) +
+    (includeMs ? '.' + time[4] : '')
+  );
+}
+
+export function dateToTimeWithMs(date: Date): string {
+  return `${date.getHours()}:${date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes()}:${date.getSeconds()>9?date.getSeconds():'0'+date.getSeconds()}.${date.getMilliseconds()}`;
 }
