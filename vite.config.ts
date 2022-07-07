c2ea3968d8a88ab911633eec14b6a60cfb980aad
Shugen <shugen002@users.noreply.github.com>
2022-07-07 20:02:40 +0800

    chore: update version generate

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { execSync } from 'child_process';
+import { inc } from 'semver';
+
+function getVersion() {
+  const desc = execSync('git describe --always --tags --abbrev=8') .toString() .trim();
+  if (desc.indexOf('-') === -1) {
+    return desc;
+  }
+  const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+  const _desc = desc.split('-');
+  const version = inc(_desc[0], 'patch');
+  const commit = _desc[1];
+  const sha = _desc[2];
+  return `${version}-${branch}.${commit}-${sha}`;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -27,7 +42,7 @@ export default defineConfig(({ command }) => {
       minify: 'esbuild',
     },
     define: {
-      '__VERSION__': command == 'build' ? '"' + require('child_process').execSync('git describe --always --tags --abbrev=8').toString().trim() + '"' : '"dev"',
+      __VERSION__: command == 'build' ? '"' + getVersion() + '"' : '"dev"',
     },
   };
 });
